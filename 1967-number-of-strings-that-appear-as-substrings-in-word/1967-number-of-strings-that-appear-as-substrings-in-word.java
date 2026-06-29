de8a2d7a5765b9e1fe39545dabae5436de58a955
Minsu Agrahari <cmminsuagrahari@gmail.com>
2026-06-29 12:29:06 +0530

    Time: 1 ms (75.75%), Space: 43.4 MB (38.69%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,32 +1,14 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
         
-        // freq Map
-        HashMap<String, Integer> freq = new HashMap<>();
+        int subCount = 0;
+
         for (String pattern : patterns) {
-            freq.put (pattern, freq.getOrDefault(pattern, 0) +1);
-        }
-
-        int patternCnt = 0;
-        int len = word.length();
-
-        for(int i=0; i<len; i++) {
-        
-            StringBuilder sb = new StringBuilder();
-
-            for (int j=i; j<len; j++) {
-                sb.append(word.charAt(j));
-                String subWord = sb.toString();
-                
-                if (freq.containsKey(subWord)) {
-                    patternCnt += freq.get(subWord);
-                    freq.remove(subWord);
-                }
+            if (word.contains(pattern)) {
+                subCount ++;
             }
-
-            sb.setLength(0);
         }
 
-        return patternCnt;
+        return subCount;
     }
 }
