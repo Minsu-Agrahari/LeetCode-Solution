a9de8af494159a7da625f1e6cdfcafde07129ce5
Minsu Agrahari <cmminsuagrahari@gmail.com>
2026-08-31 21:14:55 +0530

    Time: 27 ms (75.65%), Space: 69.1 MB (21.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,14 +2,36 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+
+function getCharCount(word) {
+    const count = new Array(26).fill(0);
+
+    for (let char of word) {
+        const index = char.charCodeAt(0) - 97;
+        count[index] ++;
+    }
+
+    return count;
+}
+
+function getKey(word) {
+    const count = getCharCount(word);
+
+    return count.join('#');
+}
+
 var groupAnagrams = function(strs) {
     
     const map = new Map();
 
-    for (const word of strs) {
-        const key = word.split("").sort().join("");
+    for (let word of strs) {
 
-        if(!map.has(key)) map.set(key, []);
+        const key = getKey(word);
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+
         map.get(key).push(word);
     }
 
